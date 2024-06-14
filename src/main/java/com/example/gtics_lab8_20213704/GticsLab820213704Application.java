6a0f9a7bfa97f8793e67f2f73d8b0219fdbeb1eb
Hineill <hineill.cespedes@pucp.edu.pe>
2024-06-13 19:14:02 -0500

    first commit

---
+++ src/main/java/com/example/gtics_lab8_20213704/GticsLab820213704Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab8_20213704;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab820213704Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab820213704Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics_lab8_20213704/GticsLab820213704ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab8_20213704;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab820213704ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
